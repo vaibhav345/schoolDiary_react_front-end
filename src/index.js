74b3079ce91f014811f2a0c68422ad851a3c9d6a
vaibhav345 <vaibhav.mahant@gmail.com>
2019-04-22 19:01:15 -0600

    Added global axios for the rest calls to Java back-end

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Axios from 'axios';
+
+{/* Default url for the Java back-end. Only this will change if the back-end url changes. */}
+Axios.defaults.baseURL = 'http://localhost:8080';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
